9c41e44711546a94fda8f981b5997e43fde50746
Prashansa <32064980+prashansahans@users.noreply.github.com>
2020-04-05 15:10:24 +0530

    Update insert-data.sql

--- sql-scripts/insert-data.sql
+++ sql-scripts/insert-data.sql
@@ -1,2 +1,5 @@
-INSERT INTO cbps 
-VALUES(1, 'Prashansa','Shreya','Ansible','Openshift')
+INSERT INTO cbps (member_id,emp_name,,mentor_name,Worked_on,currently_working)
+VALUES
+(1, 'Prashansa','Shreya','Ansible','Openshift'),
+(2, 'Utkarsh K.','Shreya','Terraform','Ansible'),
+(1, 'Neha','Shreya','Ansible','Cloudform');
