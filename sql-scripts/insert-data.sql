6c2ea8cff92655876b9c7def10f5322e3c58724d
Prashansa <32064980+prashansahans@users.noreply.github.com>
2020-05-12 15:03:56 +0530

    Update insert-data.sql

--- sql-scripts/insert-data.sql
+++ sql-scripts/insert-data.sql
@@ -2,14 +2,13 @@ USE team;
 INSERT INTO cbps
 VALUES
 (1, 'Prashansa','Shreya','Ansible','Openshift'),
-(2, 'Utkarsh K.','Shreya','Terraform','Ansible'),
-(3, 'Neha','Shreya','Ansible','Cloudform'),
-(4, 'Ramanpreet','Masarrat','Openshift','NVF+'),
-(5, 'Shubham','Pallavi J.','CLoudforms','Openshift'),
-(6, 'Mohit','Suraj','Terraform','MultiCloud'),
-(7, 'Kirtee', 'Suraj', 'Cloudforms','MultiCloud'),
-(8, 'Aishwarya','Suraj','Node js','MultiCloud'),
-(9,  'Nikhel','Suraj','Ansible','MultiCloud'),
-(10, 'Pushkar','Masarrat','Ansible','Ansible');
-(11, 'Utkarsh S.','Sadhana','Cloudoform','Cloudform'),
-(12, 'Sayontani','Pallavi J.','Cloudform','Cloudform');
+(2, 'Neha','Shreya','Ansible','Cloudform'),
+(3, 'Ramanpreet','Masarrat','Openshift','NVF+'),
+(4, 'Shubham','Pallavi J.','CLoudforms','Openshift'),
+(5, 'Mohit','Suraj','Terraform','MultiCloud'),
+(6, 'Kirtee', 'Suraj', 'Cloudforms','MultiCloud'),
+(7, 'Aishwarya','Suraj','Node js','MultiCloud'),
+(8,  'Nikhil','Suraj','Ansible','MultiCloud'),
+(9, 'Pushkar','Masarrat','Ansible','Ansible'),
+(10, 'Utkarsh S.','Sadhana','Cloudoform','Cloudform');
+
